c2b49dcb7ba6aaf30f86485d3ad7385105bf25c5
Alec Gardiner <gardin47@msu.edu>
2024-03-04 20:44:05 -0500

    Initial commit

---
+++ Pong/Pong.py
@@ -0,0 +1,24 @@
+# Pong Game in Python
+# Alec Gardiner
+
+
+import turtle
+
+win = turtle.Screen()  # initializes a window
+win.title("Pong by Alec")  # Create a title for the window
+win.bgcolor("black")  # change the background color
+win.setup(width=800, height=600)
+win.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+
+
+# Paddle B
+
+# Ball
+
+# Main game loop
+while True:
+    win.update()
+
