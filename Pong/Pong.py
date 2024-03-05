ec27d7beda31d4246d599abfb4fd2ff487a14353
Alec Gardiner <gardin47@msu.edu>
2024-03-04 21:04:59 -0500

    Added moving capability for both paddles

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -13,7 +13,7 @@ win.tracer(0)
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)  # speed of animation, set to max
-paddle_a.shape("square") # default 20 x 20 pizels
+paddle_a.shape("square")  # default 20 x 20 pixels
 paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()  # Turtle objects draw a line as they're moving, this disables that
@@ -22,7 +22,7 @@ paddle_a.goto(-350, 0)
 # Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)  # speed of animation, set to max
-paddle_b.shape("square") # default 20 x 20 pizels
+paddle_b.shape("square")  # default 20 x 20 pixels
 paddle_b.color("white")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()  # Turtle objects draw a line as they're moving, this disables that
@@ -31,13 +31,39 @@ paddle_b.goto(350, 0)
 # Ball
 ball = turtle.Turtle()
 ball.speed(0)  # speed of animation, set to max
-ball.shape("square") # default 20 x 20 pizels
+ball.shape("square")  # default 20 x 20 pixels
 ball.color("white")
 ball.penup()  # Turtle objects draw a line as they're moving, this disables that
 ball.goto(0, 0)
 
+# Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard binding
+win.listen()
+win.onkeypress(paddle_a_up, "w")
+win.onkeypress(paddle_a_down, "s")
+win.onkeypress(paddle_b_up, "Up")
+win.onkeypress(paddle_b_down, "Down")
 
 # Main game loop
 while True:
     win.update()
-
