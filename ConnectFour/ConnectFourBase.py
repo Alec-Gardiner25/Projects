eb4603a5cbc4ed567e52bfaaeaf0f04cca0a0806
Alec Gardiner <gardin47@msu.edu>
2024-03-06 16:34:45 -0500

    C4Base is almost complete. Added conditions to check if the game is won.

--- ConnectFour/ConnectFourBase.py
+++ ConnectFour/ConnectFourBase.py
@@ -3,7 +3,7 @@ import numpy as np
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 def create_board():
-    board = np.zeros((6,7))
+    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
 
 def drop_piece(board, row, col, piece):
@@ -19,6 +19,32 @@ def get_next_open_row(board, col):
         if board[r][col] == 0:
             return r
 
+def winning_move(board, piece):
+    # Check all horizontal locations for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if all(x == piece for x in board[r][c:c+4]):
+                return True
+
+    #Check vertical locations
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT,3,-1):
+            if all(x == piece for x in board[:,c][r-4:r]):
+                return True
+
+    # Check positive sloped Diagonal locations
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT,3,-1):
+            if all(x == piece for x in np.diag(np.fliplr(board[:,c:c+4][r-4:r]))):
+                return True
+
+    # check negatively sloped diagonals
+    for c in range(COLUMN_COUNT,3,-1):
+        for r in range(ROW_COUNT,3,-1):
+            if all(x == piece for x in np.diag(board[:,c-4:c][r-4:r])):
+                return True
+
+
 
 board = create_board()
 
@@ -26,15 +52,20 @@ game_over = False
 
 turn = 0
 
+winner = "Nobody"
+
 while not game_over:
     # Ask for p1 input
-    print(board)
     if turn == 0:
         col = int(input("Player 1, make your Selection (0-6): "))
 
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+            print(board)
+            game_over = winning_move(board,1)
+            if game_over:
+                winner = "Player 1"
 
     # Ask for p2 input
     else:
@@ -43,7 +74,10 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
-
+            print(board)
+            game_over = winning_move(board,2)
+            if game_over:
+                winner = "Player 2"
     turn += 1
     turn %= 2
-
+print("Game over, winner is: " , winner, "!")
