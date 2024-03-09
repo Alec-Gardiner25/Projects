580312499addfa53ce39dc438211568341416b6a
Alec Gardiner <gardin47@msu.edu>
2024-03-09 17:42:59 -0500

    creating flappy bird with neat. Added image folder to directory, imported libraries, defined constants, and loaded in all the images

---
+++ FlappyBirdAI/FlappyBird.py
@@ -0,0 +1,16 @@
+import pygame
+import neat
+import time
+import os
+import random
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+BIRD_IMAGES = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
+               pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
+               pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
