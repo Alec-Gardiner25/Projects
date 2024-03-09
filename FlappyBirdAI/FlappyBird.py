2b8abeb8f67179987114a2b3fd9b2f9783e6d23d
Alec Gardiner <gardin47@msu.edu>
2024-03-09 18:11:55 -0500

    finished bird class

--- FlappyBirdAI/FlappyBird.py
+++ FlappyBirdAI/FlappyBird.py
@@ -37,4 +37,49 @@ class Bird:
         self.height = self.y
 
     def move(self):
-        pass
+        self.tick_count += 1
+        displacement = self.vel * self.tick_count + 1.5*self.tick_count**2
+
+        if displacement >= 16:
+            displacement = 16  # set terminal velocity
+        if displacement < 0:
+            displacement -= 2 # initial 'jump'
+
+        self.y += displacement
+
+        if displacement < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
+
+    def draw(self, win):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME * 2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME * 3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME * 4:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME * 4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        if self.tilt <= -80:
+            self.img = self.IMGS[0]
+            self.img_count = self.ANIMATION_TIME * 2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topLeft=(self.x,self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+
+
+
