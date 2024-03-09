fb6f3f2822aaa536d7392c489f689356fd8859ce
Alec Gardiner <gardin47@msu.edu>
2024-03-08 19:57:42 -0500

    Finished, added high score tracking capability

--- Tetris/Tetris.py
+++ Tetris/Tetris.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import sys
 
+
 """
 10 x 20 square grid
 shapes: S, Z, I, O, J, L, T
@@ -271,7 +272,26 @@ def draw_next_shape(shape, surface):
 
     surface.blit(label, (sx+10, sy - 30))
 
-def draw_window(surface,grid, score=0):
+def update_score(nscore):
+    with open('scores.txt', 'r') as f:
+        lines = f.readlines()
+        score = lines[0].strip()
+
+    with open('scores.txt', 'w') as f:
+        if int(score) > int(nscore):
+            f.write(str(score))
+        else:
+            f.write(str(nscore))
+
+
+def max_score():
+    with open('scores.txt', 'r') as f:
+        lines = f.readlines()
+        score = lines[0].strip()
+    return score
+
+
+def draw_window(surface,grid, score=0, last_score=0):
     surface.fill((0,0,0))
     font = pygame.font.SysFont("comicsans", 60)
     label = font .render('Tetris', 1, (255, 255, 255))
@@ -284,11 +304,19 @@ def draw_window(surface,grid, score=0):
 
     pygame.draw.rect(surface, (255,0,0), (top_left_x, top_left_y, play_width, play_height), 4)
 
+    # current score
     font = pygame.font.SysFont('comicsans', 30)
     label = font.render('Score: ' + str(score), 2, (255,255,255))
 
     sx = top_left_x + play_width + 50
     sy = top_left_y + play_height/2 - 100
+
+    surface.blit(label, (sx + 20, sy + 160))
+
+    label = font.render('High Score: ' + str(last_score), 2, (255,255,255))
+
+    sx = top_left_x - 250
+    sy = top_left_y + 200
 
     surface.blit(label, (sx + 20, sy + 160))
 
@@ -299,6 +327,7 @@ def draw_window(surface,grid, score=0):
 
 
 def main(win):
+    last_score = max_score()
     locked_positions = {}
     grid = create_grid(locked_positions)
     change_piece = False
@@ -318,10 +347,10 @@ def main(win):
         level_time += clock.get_rawtime()
         clock.tick()
 
-        #if level_time/1000 > 5:
-            #level_time = 0
-            #if fall_speed > 0.12:
-                #fall_speed -= .005
+        if level_time/1000 > 5:
+            level_time = 0
+            if fall_speed > 0.12:
+                fall_speed -= .005
 
         if fall_time / 1000 > fall_speed:
             fall_time = 0
@@ -369,8 +398,7 @@ def main(win):
             change_piece = False
             score += clear_rows(grid, locked_positions) * 5
 
-
-        draw_window(win,grid,score)
+        draw_window(win,grid,score, last_score)
         draw_next_shape(next_piece,win)
         pygame.display.update()
 
@@ -379,6 +407,7 @@ def main(win):
             pygame.display.update()
             pygame.time.delay(1500)
             run = False
+            update_score(score)
 
 
 def main_menu():
